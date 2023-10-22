410ce65340ace9e410768a3ecf9b733e0226950c
varga <varga@PAUL>
2023-10-22 20:27:35 +0300

    Add project files.

---
+++ Data/Varga_Paul_Lab2_0Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Varga_Paul_Lab2_0.Models;
+
+namespace Varga_Paul_Lab2_0.Data
+{
+    public class Varga_Paul_Lab2_0Context : DbContext
+    {
+        public Varga_Paul_Lab2_0Context (DbContextOptions<Varga_Paul_Lab2_0Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Varga_Paul_Lab2_0.Models.Book> Book { get; set; } = default!;
+    }
+}
